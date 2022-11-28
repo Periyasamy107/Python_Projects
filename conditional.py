2ce57d81f700419e8036f265ad980dd45d163d5d
Periyasamy107 <cperiyasamy085@gmail.com>
2022-11-28 20:02:37 +0530

    conditional commit

---
+++ conditional.py
@@ -0,0 +1,59 @@
+ 
+'''  a = 100
+b = 400
+
+if a>b:
+    print ( "a is big")
+else:
+    print( "b is big")  '''
+
+''' age = 15
+
+if age>=18:
+    print("vote eligible")
+else: 
+    print("not eligible")  '''
+
+'''  suresh_total = 476
+ratha_total = 495
+deena_total = 349
+sam_total = 480
+
+if sam_total>suresh_total or sam_total>ratha_total or sam_total>deena_total:
+    print('sam got high score')
+elif deena_total>suresh_total or deena_total>ratha_total or deena_total>sam_total:
+    print('deena got high score')
+elif ratha_total>suresh_total or ratha_total>deena_total or ratha_total>sam_total:
+    print('ratha got high score')
+else:
+    print('suresh got high score') '''
+
+
+'''  a = 50
+
+if a<=10: 
+    print('between 10')
+elif a<=50:
+    print('between 50')
+elif a<=100:
+    print('between 100')
+elif a<=150:
+    print('between 150')
+else:
+    print("greater than 150")   '''
+
+
+""" a = 68
+b = 78
+c = 98
+d = 98
+
+if a>=b and a>=c and a>=d:
+    print ('a is equal to or greater than b,c,d')
+elif b>=c and b>=d and b>=a:
+    print ('b is equal to or greater than a,c,d')
+elif c>=a and c>=b and c>=d:
+    print ('c is equal to or greater than a,b,d')
+else:
+    print('d is equal to or greater than a,b,c') """
+
