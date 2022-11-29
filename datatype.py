78e637bdddbf6d7caaf316633b160ad72de5c2b4
Periyasamy107 <cperiyasamy085@gmail.com>
2022-11-29 11:08:09 +0530

    datatype commit

---
+++ datatype.py
@@ -0,0 +1,34 @@
+""" a=22
+b=5 """
+#print(a+b)
+#print(a-b)
+#print(a*b)
+#print(a/b)
+"""print(a%b)
+print(a//b)
+print(a**b)
+print(a^b)"""
+
+#if(5>1):
+#print("sam")
+
+""" a=int(5)
+b=str(6)
+c=float(7)
+
+print(a,b,c)
+
+print(type(a))
+print(type(b))
+print(type(c)) """
+
+# int  => 1,2,-4,0
+# float  => 4.56,6768.345
+# string => 'sam',"sam"
+# boolean => True,False
+# list => [1,3.4,True,'sam']
+# tuple => (1,7.53,'sam',False)
+# dictionay => {'name':'sam','age':345,'salary':10000}
+
+
+
