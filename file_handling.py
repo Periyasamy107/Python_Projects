d4ad5a3ccb5fc68a759b660caa7a8a656f495228
Periyasamy107 <cperiyasamy085@gmail.com>
2022-12-01 17:49:01 +0530

    opening another file in file handling folder

--- file_handling.py
+++ file_handling.py
@@ -119,4 +119,7 @@ with open(filename,'r') as file:
     print(type(result)) 
     print(sum(result)) """
 
+filename = 'sample.txt'
+with open(filename,'w') as file:
+    file.write('I am a good boy')
 
