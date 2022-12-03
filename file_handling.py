af5e5eb375b8a34fd2c11fd77924c030de1ff73e
Periyasamy107 <cperiyasamy085@gmail.com>
2022-12-03 16:12:45 +0530

    added one comment

--- file_handling.py
+++ file_handling.py
@@ -119,7 +119,7 @@ with open(filename,'r') as file:
     print(type(result)) 
     print(sum(result)) """
 
-filename = 'sample.txt'
+""" filename = 'sample.txt'
 with open(filename,'w') as file:
-    file.write('I am a good boy')
+    file.write('I am a good boy') """
 
