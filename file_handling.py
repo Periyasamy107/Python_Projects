d923b3e8843290f278adf11252d75b33f8337117
Periyasamy107 <cperiyasamy085@gmail.com>
2022-12-18 07:49:48 +0530

    a+ operation performed in file handling with test file

--- file_handling.py
+++ file_handling.py
@@ -336,3 +336,10 @@ file.close() """
         file.write(str(i)+' ')
         a = file.read()
         print(a) """  
+
+""" with open('test.txt','a+') as file:
+    for i in range(1,6):
+        file.write(str(i)+'--')
+        a = file.read()
+        print(a) """
+
