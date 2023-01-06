c2c1e5261eca4531995e69cf2b1ef0a47ff7d0fd
Periyasamy107 <cperiyasamy085@gmail.com>
2023-01-06 11:32:20 +0530

    class inheritance practiced

--- class_objects.py
+++ class_objects.py
@@ -10,3 +10,14 @@ print(f'car speed : {car.speed}, car mile : {car.mileage}')
 print(bus.speed,bus.mileage)
 print(bike.speed,bike.mileage) """
 
+""" class Vehicle:
+    def __init__(self,speed,mileage):
+        self.speed=speed
+        self.mileage=mileage 
+
+    def car(Vehicle):
+        pass 
+
+car = Vehicle(150,18,)
+print('car speed :',car.speed,'car mile :',car.mileage) """
+
