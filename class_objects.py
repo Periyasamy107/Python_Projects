aa4443d314c8dd92d489e18d669d8dfd8d32c128
Periyasamy107 <cperiyasamy085@gmail.com>
2023-01-06 11:25:15 +0530

    create a class with instance attributes

---
+++ class_objects.py
@@ -0,0 +1,12 @@
+""" class Vehicle:
+    def __init__(self,speed,mileage):
+        self.speed=speed
+        self.mileage=mileage 
+car = Vehicle(150,18)
+bus = Vehicle(100,28)
+bike = Vehicle(300,10)
+
+print(f'car speed : {car.speed}, car mile : {car.mileage}')
+print(bus.speed,bus.mileage)
+print(bike.speed,bike.mileage) """
+
