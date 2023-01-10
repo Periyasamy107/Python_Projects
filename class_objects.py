37210548de76ef9909df453c755e9b4add3e0fb0
Periyasamy107 <cperiyasamy085@gmail.com>
2023-01-10 10:29:09 +0530

    pass object as an argument to the class

--- class_objects.py
+++ class_objects.py
@@ -131,3 +131,23 @@ temp = vars(animal)
 for item in temp:
     print(item, ':', temp[item]) """
 
+
+# class Vehicle:
+#     def __init__(self):
+#         self.trucks = []
+#     def add_truck(self, truck):
+#         self.trucks.append(truck)
+# class Truck:
+#     def __init__(self,color):
+#         self.color = color
+#     def __repr__(self):
+#         return '{}'.format(self.color)
+# def main():
+#     v = Vehicle()
+#     for t in 'Yellow Orange Pink Rose Gold'.split():
+#         t = Truck(t)
+#         v.add_truck(t)
+#     print(v.trucks)
+# if __name__ == "__main__":
+#     main()
+
