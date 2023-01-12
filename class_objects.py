0c9b7b6799080fd8967e547c119f09c8886cac1e
Periyasamy107 <cperiyasamy085@gmail.com>
2023-01-12 09:03:24 +0530

    Reversing a string by using Iterator

--- class_objects.py
+++ class_objects.py
@@ -223,4 +223,18 @@ sample() """
 for i in Counter(2,4):
     print(i) """
 
+""" class Reverse:
+    def __init__(self,data):
+        self.data = data 
+        self.index = len(data)
+    def __iter__(self):
+        return self 
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index - 1 
+        return self.data[self.index]
+test = Reverse('samy')
+for i in test:
+    print(i,end='') """
 
