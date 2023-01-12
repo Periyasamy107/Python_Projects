08a1838038d20a918cdb2c77adede0ed55ee8f7c
Periyasamy107 <cperiyasamy085@gmail.com>
2023-01-12 08:56:24 +0530

    iteration overload practiced by using class and methods

--- class_objects.py
+++ class_objects.py
@@ -207,5 +207,20 @@ sample()
 sample()
 sample() """
 
+""" class Counter:
+    def __init__(self,low,high):
+        self.low = low 
+        self.high = high 
+    def __iter__(self):
+        return self 
+    def __next__(self):
+        if self.low > self.high:
+            raise StopIteration
+        else:
+            self.low+=1
+            return self.low-1 
+
+for i in Counter(2,4):
+    print(i) """
 
 
